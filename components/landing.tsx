74c102e54c7708f48dbd89e8507c02da28b74818
M-Jey <mjeyict3@gmail.com>
2025-03-07 16:00:06 +0800

    Add initial setup for NativeWind and Tailwind CSS integration

---
+++ components/landing.tsx
@@ -0,0 +1,10 @@
+import "nativewind";
+import { View, Text } from "react-native";
+
+export default function Landing() {
+  return (
+    <View className="flex-1 bg-[#2D2D2D] items-center w-[100%]">
+      <Text className="text-white text-2xl mt-10">ShoeCase</Text>
+    </View>
+  )
+}
