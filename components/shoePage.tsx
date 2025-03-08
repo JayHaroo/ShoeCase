ab6a5f3a9021ae3307099e9941138671a73616dd
M-Jey <mjeyict3@gmail.com>
2025-03-08 13:36:22 +0800

    Enhance ShoePage component with image display and navigation back to Landing

--- components/shoePage.tsx
+++ components/shoePage.tsx
@@ -1,22 +1,37 @@
-import { View, Text, Pressable } from "react-native";
+import { View, Text, Pressable, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 
 export default function ShoePage() {
+  const navigation = useNavigation();
 
-    const navigation = useNavigation();
-
-    return (
-        <>
-        <View className="flex-1 bg-[#121212] items-center w-[100%]">
-            <View className="justify-center items-center flex flex-row justify-between w-[90%]">
-                <Text className="text-white mt-10 text-2xl font-inter">Details</Text>
-                <Pressable className="mt-10 p-2 rounded-2xl w-[120px] items-center border-2 border-white"
-                    onPress={() => navigation.navigate("Landing")}> 
-                    <Text className="text-white font-inter">Back</Text>
-                </Pressable>
-            </View>
+  return (
+    <>
+      <View className="flex-1 bg-[#121212] items-center w-[100%]">
+        {/* This part will contain the top navigation bar of the app */}
+        <View className="justify-center items-center flex flex-row justify-between w-[90%]">
+          <Text className="text-white mt-10 text-2xl font-inter font-bold">Details</Text>
+          <Pressable
+            className="mt-10 p-2 rounded-2xl w-[120px] items-center border-2 border-white"
+            onPress={() => navigation.navigate("Landing")}
+          >
+            <Text className="text-white font-inter">Back</Text>
+          </Pressable>
         </View>
-        </>
-    )
+
+        {/* This part will contain the image holder */}
+        <View className="mt-10 w-[90%] h-[300px] bg-white rounded-2xl">
+            <Image
+              source={require("../assets/samples/aja1.jpg")} 
+              className="w-[100%] h-[100%] object-contain"/>
+        </View>
+
+        {/* This part will contain the shoe header details */}
+        <View className="mt-10 w-[90%] flex flex-row justify-between align-middle items-center">
+          <Text className="text-white text-3xl font-inter font-bold">A`Ja One</Text>
+          <Text className="text-white text-2xl font-inter">Nike</Text>
+        </View>
+      </View>
+    </>
+  );
 }
